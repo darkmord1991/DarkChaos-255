e189471d4a8d6f8b16fa08634ae103664afe9066
Darkmord1991 <florian1@pwschmidt.de>
2025-09-18 22:07:57 +0200

    a few updates

--- Custom/Level Areas/Ashzara Crater 1 - 80/Start NPCs - standard ones.sql
+++ Custom/Level Areas/Ashzara Crater 1 - 80/Start NPCs - standard ones.sql
@@ -78,3 +78,9 @@ UPDATE `creature_template` SET `faction` = 35 WHERE (`entry` = 2134);
 UPDATE `creature_template` SET `faction` = 35 WHERE (`entry` = 2136);
 UPDATE `creature_template` SET `faction` = 35 WHERE (`entry` = 2135);
 UPDATE `creature_template` SET `faction` = 35 WHERE (`entry` = 10055);
+
+-- butcher from crossroads
+DELETE FROM `creature_template` WHERE (`entry` = 3489);
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(3489, 0, 0, 0, 0, 0, 'Zargh', 'Butcher', NULL, 4341, 16, 16, 0, 35, 643, 1, 1.14286, 1, 1, 18, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 512, 2048, 0, 0, 0, 0, 0, 0, 7, 134217728, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 2, '', 12340);
+
