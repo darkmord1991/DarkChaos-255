fc5a0309e0ee65a62638b9f5d6c3ee6116cbe5ec
Darkmord1991 <florian1@pwschmidt.de>
2025-09-19 20:35:25 +0200

    vendors fixed +  some more item updates, still not balanced

--- Custom/Custom Items/Itemsets/T11/Item Description update.sql
+++ Custom/Custom Items/Itemsets/T11/Item Description update.sql
@@ -1,1 +1,3 @@
 UPDATE `item_template` SET `description` = 'T11' WHERE entry >= 81000 and entry <= 81100;
+
+UPDATE `item_template` SET `itemlevel` = '500' WHERE entry >= 81000 and entry <= 81100;
--- Custom/Custom Items/Itemsets/T12/Item Description T12.sql
+++ Custom/Custom Items/Itemsets/T12/Item Description T12.sql
@@ -1,1 +1,3 @@
 UPDATE `item_template` SET `description` = 'T12' WHERE entry >= 81100 and entry <= 81200;
+
+UPDATE `item_template` SET `itemlevel` = '510' WHERE entry >= 81100 and entry <= 81200;
