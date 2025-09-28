7ea68845db011a0d79488ebf3119e8e69cdbffe3
Darkmord1991 <florian1@pwschmidt.de>
2025-09-28 19:08:25 +0200

    login announce from lua to c++

---
+++ src/server/scripts/DC/dc_login_announce.cpp
@@ -0,0 +1,76 @@
+#include "ScriptMgr.h"
+#include "Chat.h"
+#include "Player.h"
+#include "World.h"
+#include <array>
+#include <string>
+
+namespace
+{
+    // Class color hexes by Class enum index (1..11 used here)
+    static constexpr std::array<char const*, 12> kClassHex = {
+        "",         // 0 unused
+        "C79C6E",  // 1 Warrior
+        "F58CBA",  // 2 Paladin
+        "ABD473",  // 3 Hunter
+        "FFF569",  // 4 Rogue
+        "FFFFFF",  // 5 Priest
+        "C41F3B",  // 6 Death Knight
+        "0070DE",  // 7 Shaman
+        "69CCF0",  // 8 Mage
+        "9482C9",  // 9 Warlock
+        "",         // 10 (unused in WotLK: Monk)
+        "FF7d0A"   // 11 Druid
+    };
+
+    static std::string MakeClassColoredName(Player* player)
+    {
+        uint8 cls = player->GetClass();
+        char const* hex = (cls < kClassHex.size() && kClassHex[cls] && *kClassHex[cls]) ? kClassHex[cls] : "FFFFFF";
+        // |cffHEX|Hplayer:|h [Name]|h|r
+        std::string out = "|cff";
+        out += hex;
+        out += "|Hplayer:|h [";
+        out += player->GetName();
+        out += "]|h|r";
+        return out;
+    }
+}
+
+class DC_LoginAnnounce : public PlayerScript
+{
+public:
+    DC_LoginAnnounce() : PlayerScript("DC_LoginAnnounce") { }
+
+    void OnLogin(Player* player) override
+    {
+        std::string colored = MakeClassColoredName(player);
+        std::string suffix;
+        if (player->IsGameMaster())
+            suffix = " - |cffDF01D7[Owner]|h|r has logged in."; // matches lua color/tag
+        else if (player->IsHorde())
+            suffix = " - |cff610B0B[Horde]|h|r has logged in.";
+        else
+            suffix = " - |cff0101DF[Alliance]|h|r has logged in.";
+
+        sWorld->SendServerMessage(SERVER_MSG_STRING, (colored + suffix).c_str());
+    }
+
+    void OnLogout(Player* player) override
+    {
+        // Simpler logout line like the Lua version
+        std::string tag;
+        if (player->IsGameMaster())
+            tag = "GM";
+        else
+            tag = player->IsHorde() ? "Horde" : "Alliance";
+
+        std::string msg = "[" + player->GetName() + " - " + tag + "] has logged out.";
+        sWorld->SendServerMessage(SERVER_MSG_STRING, msg.c_str());
+    }
+};
+
+void AddSC_dc_login_announce()
+{
+    new DC_LoginAnnounce();
+}
--- src/server/scripts/DC/dc_script_loader.cpp
+++ src/server/scripts/DC/dc_script_loader.cpp
@@ -20,6 +20,7 @@ void AddSC_ac_guard_npc(); // location: scripts\DC\AC\ac_guard_npc.cpp (C++ link
 void AddSC_npc_thrall_hinterlandbg(); // location: scripts\DC\HinterlandBG\npc_thrall_warchief.cpp
 void AddSC_hinterlandbg_Varian_wrynn(); // location: scripts\DC\HinterlandBG\npc_Varian_hinterlandbg.cpp
 void AddSC_hlbg_commandscript(); // location: scripts\DC\HinterlandBG\hlbg_commandscript.cpp (C++ linkage)
+void AddSC_dc_login_announce(); // location: scripts\DC\dc_login_announce.cpp
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
@@ -29,4 +30,5 @@ void AddDCScripts()
     AddSC_npc_thrall_hinterlandbg();
     AddSC_hinterlandbg_Varian_wrynn();
     AddSC_hlbg_commandscript();
+    AddSC_dc_login_announce();
 }
