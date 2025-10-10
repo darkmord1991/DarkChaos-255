e20da7e961a0e72344f98d690682de6220030c91
Darkmord1991 <florian1@pwschmidt.de>
2025-10-10 10:04:19 +0200

    .

--- src/server/scripts/DC/AC/ac_flightmasters.cpp
+++ src/server/scripts/DC/AC/ac_flightmasters.cpp
@@ -1361,8 +1361,9 @@ public:
     }
 };
 
-} ; // namespace DC_AC_Flight
+}
 
+// Close namespace DC_AC_Flight
 void AddSC_flightmasters()
 {
     // Register the flightmaster scripts using unqualified names resolved into the namespace.
