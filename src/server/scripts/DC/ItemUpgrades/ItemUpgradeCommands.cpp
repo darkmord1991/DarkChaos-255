7b550d5a33a7a246021816feeaf49e0353cccf73
Darkmord1991 <florian1@pwschmidt.de>
2025-11-06 23:52:43 +0100

    .

--- src/server/scripts/DC/ItemUpgrades/ItemUpgradeCommands.cpp
+++ src/server/scripts/DC/ItemUpgrades/ItemUpgradeCommands.cpp
@@ -1,22 +1,28 @@
 #include "ScriptMgr.h"
 #include "Chat.h"
 #include "Player.h"
+#include "Item.h"
 #include "DatabaseEnv.h"
+#include <sstream>
+
+using Acore::ChatCommands::ChatCommandBuilder;
+using Acore::ChatCommands::Console;
 
 class ItemUpgradeCommands : public CommandScript
 {
 public:
     ItemUpgradeCommands() : CommandScript("ItemUpgradeCommands") { }
 
-    std::vector<ChatCommand> GetCommands() const override
+    [[nodiscard]] std::vector<ChatCommandBuilder> GetCommands() const override
     {
-        static std::vector<ChatCommand> dcupgradeCommandTable =
+        static const std::vector<ChatCommandBuilder> dcupgradeCommandTable =
         {
-            { "dcupgrade", SEC_PLAYER, true, &HandleItemUpgradeCommand, "" },
+            ChatCommandBuilder("dcupgrade", HandleItemUpgradeCommand, Console::No),
         };
         return dcupgradeCommandTable;
     }
 
+private:
     static bool HandleItemUpgradeCommand(ChatHandler* handler, const char* args)
     {
         Player* player = handler->GetSession()->GetPlayer();
@@ -24,11 +30,12 @@ public:
             return false;
 
         // Parse arguments
-        char* arg1 = strtok((char*)args, " ");
-        if (!arg1)
+        std::string argStr = args ? args : "";
+        std::string::size_type spacePos = argStr.find(' ');
+        std::string subcommand = spacePos != std::string::npos ? argStr.substr(0, spacePos) : argStr;
+        
+        if (subcommand.empty())
             return false;
-
-        std::string subcommand = arg1;
 
         // INIT: Get player's current currency
         if (subcommand == "init")
@@ -36,44 +43,39 @@ public:
             uint32 playerGuid = player->GetGUIDLow();
             
             QueryResult tokens_result = CharacterDatabase.Query(
-                "SELECT amount FROM dc_item_upgrade_currency WHERE player_guid = {} AND currency_type = 1",
+                "SELECT amount FROM dc_item_upgrade_currency WHERE player_guid = %u AND currency_type = 1",
                 playerGuid
             );
-            uint32 tokens = tokens_result ? tokens_result->Fetch()[0].Get<uint32>() : 0;
+            uint32 tokens = tokens_result ? (*tokens_result)[0].Get<uint32>() : 0;
 
             QueryResult essence_result = CharacterDatabase.Query(
-                "SELECT amount FROM dc_item_upgrade_currency WHERE player_guid = {} AND currency_type = 2",
+                "SELECT amount FROM dc_item_upgrade_currency WHERE player_guid = %u AND currency_type = 2",
                 playerGuid
             );
-            uint32 essence = essence_result ? essence_result->Fetch()[0].Get<uint32>() : 0;
+            uint32 essence = essence_result ? (*essence_result)[0].Get<uint32>() : 0;
 
             // Send response as system message
-            player->GetSession()->SendNotification(
-                "DCUPGRADE_INIT:%u:%u",
-                tokens, essence
-            );
+            handler->PSendSysMessage("DCUPGRADE_INIT:%u:%u", tokens, essence);
             return true;
         }
 
         // QUERY: Get item upgrade info
         else if (subcommand == "query")
         {
-            char* bagStr = strtok(nullptr, " ");
-            char* slotStr = strtok(nullptr, " ");
-
-            if (!bagStr || !slotStr)
+            std::string remainingArgs = spacePos != std::string::npos ? argStr.substr(spacePos + 1) : "";
+            std::istringstream iss(remainingArgs);
+            uint32 bag, slot;
+            
+            if (!(iss >> bag >> slot))
             {
-                player->GetSession()->SendNotification("DCUPGRADE_ERROR:Invalid parameters");
+                handler->PSendSysMessage("DCUPGRADE_ERROR:Invalid parameters");
                 return true;
             }
-
-            uint32 bag = atoi(bagStr);
-            uint32 slot = atoi(slotStr);
 
             Item* item = player->GetItemByPos(bag, slot);
             if (!item)
             {
-                player->GetSession()->SendNotification("DCUPGRADE_ERROR:Item not found");
+                handler->PSendSysMessage("DCUPGRADE_ERROR:Item not found");
                 return true;
             }
 
@@ -81,7 +83,7 @@ public:
             uint32 baseItemLevel = item->GetItemLevel(player);
 
             QueryResult result = CharacterDatabase.Query(
-                "SELECT upgrade_level, tier FROM dc_item_upgrade_state WHERE item_guid = {}",
+                "SELECT upgrade_level, tier FROM dc_item_upgrade_state WHERE item_guid = %u",
                 itemGUID
             );
 
@@ -90,8 +92,8 @@ public:
 
             if (result)
             {
-                upgradeLevel = result->Fetch()[0].Get<uint32>();
-                tier = result->Fetch()[1].Get<uint32>();
+                upgradeLevel = (*result)[0].Get<uint32>();
+                tier = (*result)[1].Get<uint32>();
             }
             else
             {
@@ -103,40 +105,33 @@ public:
                 else tier = 1;
             }
 
-            player->GetSession()->SendNotification(
-                "DCUPGRADE_QUERY:%u:%u:%u:%u",
-                itemGUID, upgradeLevel, tier, baseItemLevel
-            );
+            handler->PSendSysMessage("DCUPGRADE_QUERY:%u:%u:%u:%u", itemGUID, upgradeLevel, tier, baseItemLevel);
             return true;
         }
 
         // PERFORM: Perform upgrade
         else if (subcommand == "perform")
         {
-            char* bagStr = strtok(nullptr, " ");
-            char* slotStr = strtok(nullptr, " ");
-            char* levelStr = strtok(nullptr, " ");
-
-            if (!bagStr || !slotStr || !levelStr)
+            std::string remainingArgs = spacePos != std::string::npos ? argStr.substr(spacePos + 1) : "";
+            std::istringstream iss(remainingArgs);
+            uint32 bag, slot, targetLevel;
+            
+            if (!(iss >> bag >> slot >> targetLevel))
             {
-                player->GetSession()->SendNotification("DCUPGRADE_ERROR:Invalid parameters");
+                handler->PSendSysMessage("DCUPGRADE_ERROR:Invalid parameters");
                 return true;
             }
-
-            uint32 bag = atoi(bagStr);
-            uint32 slot = atoi(slotStr);
-            uint32 targetLevel = atoi(levelStr);
 
             Item* item = player->GetItemByPos(bag, slot);
             if (!item)
             {
-                player->GetSession()->SendNotification("DCUPGRADE_ERROR:Item not found");
+                handler->PSendSysMessage("DCUPGRADE_ERROR:Item not found");
                 return true;
             }
 
             if (targetLevel > 15)
             {
-                player->GetSession()->SendNotification("DCUPGRADE_ERROR:Max level is 15");
+                handler->PSendSysMessage("DCUPGRADE_ERROR:Max level is 15");
                 return true;
             }
 
@@ -146,7 +141,7 @@ public:
 
             // Get current upgrade state
             QueryResult stateResult = CharacterDatabase.Query(
-                "SELECT upgrade_level, tier FROM dc_item_upgrade_state WHERE item_guid = {}",
+                "SELECT upgrade_level, tier FROM dc_item_upgrade_state WHERE item_guid = %u",
                 itemGUID
             );
 
@@ -155,8 +150,8 @@ public:
 
             if (stateResult)
             {
-                currentLevel = stateResult->Fetch()[0].Get<uint32>();
-                tier = stateResult->Fetch()[1].Get<uint32>();
+                currentLevel = (*stateResult)[0].Get<uint32>();
+                tier = (*stateResult)[1].Get<uint32>();
             }
             else
             {
@@ -169,80 +164,71 @@ public:
 
             if (targetLevel <= currentLevel)
             {
-                player->GetSession()->SendNotification("DCUPGRADE_ERROR:Must upgrade to higher level");
+                handler->PSendSysMessage("DCUPGRADE_ERROR:Must upgrade to higher level");
                 return true;
             }
 
             // Get upgrade cost
             QueryResult costResult = WorldDatabase.Query(
-                "SELECT upgrade_tokens, artifact_essence FROM dc_item_upgrade_costs WHERE tier = {} AND upgrade_level = {}",
+                "SELECT upgrade_tokens, artifact_essence FROM dc_item_upgrade_costs WHERE tier = %u AND upgrade_level = %u",
                 tier, targetLevel
             );
 
             if (!costResult)
             {
-                player->GetSession()->SendNotification("DCUPGRADE_ERROR:Cost not found");
+                handler->PSendSysMessage("DCUPGRADE_ERROR:Cost not found");
                 return true;
             }
 
-            uint32 tokensNeeded = costResult->Fetch()[0].Get<uint32>();
-            uint32 essenceNeeded = costResult->Fetch()[1].Get<uint32>();
+            uint32 tokensNeeded = (*costResult)[0].Get<uint32>();
+            uint32 essenceNeeded = (*costResult)[1].Get<uint32>();
 
             // Check currency
             QueryResult tokenResult = CharacterDatabase.Query(
-                "SELECT amount FROM dc_item_upgrade_currency WHERE player_guid = {} AND currency_type = 1",
+                "SELECT amount FROM dc_item_upgrade_currency WHERE player_guid = %u AND currency_type = 1",
                 playerGuid
             );
-            uint32 currentTokens = tokenResult ? tokenResult->Fetch()[0].Get<uint32>() : 0;
+            uint32 currentTokens = tokenResult ? (*tokenResult)[0].Get<uint32>() : 0;
 
             QueryResult essenceResult = CharacterDatabase.Query(
-                "SELECT amount FROM dc_item_upgrade_currency WHERE player_guid = {} AND currency_type = 2",
+                "SELECT amount FROM dc_item_upgrade_currency WHERE player_guid = %u AND currency_type = 2",
                 playerGuid
             );
-            uint32 currentEssence = essenceResult ? essenceResult->Fetch()[0].Get<uint32>() : 0;
+            uint32 currentEssence = essenceResult ? (*essenceResult)[0].Get<uint32>() : 0;
 
             if (currentTokens < tokensNeeded)
             {
-                player->GetSession()->SendNotification(
-                    "DCUPGRADE_ERROR:Need %u tokens, have %u",
-                    tokensNeeded, currentTokens
-                );
+                handler->PSendSysMessage("DCUPGRADE_ERROR:Need %u tokens, have %u", tokensNeeded, currentTokens);
                 return true;
             }
 
             if (currentEssence < essenceNeeded)
             {
-                player->GetSession()->SendNotification(
-                    "DCUPGRADE_ERROR:Need %u essence, have %u",
-                    essenceNeeded, currentEssence
-                );
+                handler->PSendSysMessage("DCUPGRADE_ERROR:Need %u essence, have %u", essenceNeeded, currentEssence);
                 return true;
             }
 
             // Deduct currency
             CharacterDatabase.Execute(
-                "UPDATE dc_item_upgrade_currency SET amount = amount - {} WHERE player_guid = {} AND currency_type = 1",
+                "UPDATE dc_item_upgrade_currency SET amount = amount - %u WHERE player_guid = %u AND currency_type = 1",
                 tokensNeeded, playerGuid
             );
 
             CharacterDatabase.Execute(
-                "UPDATE dc_item_upgrade_currency SET amount = amount - {} WHERE player_guid = {} AND currency_type = 2",
+                "UPDATE dc_item_upgrade_currency SET amount = amount - %u WHERE player_guid = %u AND currency_type = 2",
                 essenceNeeded, playerGuid
             );
 
             // Update item state
             CharacterDatabase.Execute(
                 "INSERT INTO dc_item_upgrade_state (item_guid, player_guid, upgrade_level, tier, tokens_invested) "
-                "VALUES ({}, {}, {}, {}, {}) "
-                "ON DUPLICATE KEY UPDATE upgrade_level = {}, tokens_invested = tokens_invested + {}",
+                "VALUES (%u, %u, %u, %u, %u) "
+                "ON DUPLICATE KEY UPDATE upgrade_level = %u, tokens_invested = tokens_invested + %u",
                 itemGUID, playerGuid, targetLevel, tier, tokensNeeded, targetLevel, tokensNeeded
             );
 
-            player->GetSession()->SendNotification(
-                "DCUPGRADE_SUCCESS:%u:%u",
-                itemGUID, targetLevel
-            );
-            player->GetSession()->SendNotification("|cff00ff00Item upgraded to level %u!", targetLevel);
+            handler->PSendSysMessage("DCUPGRADE_SUCCESS:%u:%u", itemGUID, targetLevel);
+            handler->PSendSysMessage("|cff00ff00Item upgraded to level %u!", targetLevel);
 
             return true;
         }
