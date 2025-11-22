243cf1dcb24269934640e83bcd397636b7df5b82
Darkmord1991 <florian1@pwschmidt.de>
2025-11-22 20:14:13 +0100

    .

--- src/server/scripts/Commands/cs_hl_bg.cpp
+++ src/server/scripts/Commands/cs_hl_bg.cpp
@@ -100,7 +100,7 @@ public:
             { "results", HandleHLBGResults, SEC_GAMEMASTER, Console::No },
 
             // Nested 'queue' subtable for player queue actions
-            { "queue", queueSubTable },
+            { "queue", queueSubTable }
         };
 
         static ChatCommandTable commandTable =
